6aedb871d279fccbc933b675b5ff03571a0573d1
251370232-design <251370232@gift.edu.pk>
2026-04-01 00:05:10 +0500

    Add files via upload

---
+++ DigitsLab4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class DigitsLab4 {
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the First number: ");
+         int m1 = input.nextInt();
+
+        System.out.print("Enter the Second number: ");
+        int f1 = input.nextInt();
+
+        System.out.print("Enter the Third number: ");
+        int m2 = input.nextInt();
+
+        System.out.print("Enter the Fourth number: ");
+        int  f2 = input.nextInt();
+
+        System.out.print("Enter the Fifth number: ");
+        int m3 = input.nextInt();
+
+        System.out.println(m1+" "+f1+" "+m2+" "+f2+" "+m3);
+        
+    }
+}
